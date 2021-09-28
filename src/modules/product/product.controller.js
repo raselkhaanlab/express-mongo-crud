bb8c4f32394b57eaf24a4a81f9700867a4c83b50
Rasel Khan <rasel.khan@jmrltd.com>
2021-09-28 15:49:00 +0600

    add product update

--- src/modules/product/product.controller.js
+++ src/modules/product/product.controller.js
@@ -9,6 +9,45 @@ let productModel = require('./model/product.model');
     catch(e){next(e)}
   };
 
+  exports.get = async (req,res,next) =>{
+    try{
+      const id = req.params.id;
+      const isValidId=mongoose.isValidObjectId(id);
+      if(!isValidId) {
+        return res.status(400).json({errors:`${id} is not a valid id` })
+      }
+      const data = await productModel.findById(id,{__v:0});
+      if(data) return res.json(data);
+      return res.status(404).json();
+    }
+    catch(e){
+      console.log(e);
+      next(e);
+    }
+  };
+
+  exports.update = async (req,res,next) =>{
+    
+    try{
+      const id = req.params.id;
+      const isValidId=mongoose.isValidObjectId(id);
+      if(!isValidId) {
+        return res.status(400).json({errors:`${id} is not a valid id` })
+      }
+      
+      const data = await productModel.findByIdAndUpdate(id,{$set:{...req.body}},{new:true});
+      if(data) {
+        const {__v,...modData} = data._doc;
+        return res.json(modData);
+      }
+      return res.status(404).json();
+    }
+    catch(e){
+      console.log(e);
+      next(e);
+    }
+  };
+
 //save product
 
 exports.save = async (req,res,next) =>{
--- src/modules/product/product.routes.js
+++ src/modules/product/product.routes.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const productController = require('./product.controller');
 // define the home page route
+router.get('/:id',productController.get);
+router.put('/:id',productController.update);
 router.get('/', productController.list);
 router.post('/',productController.save);
 module.exports = router;
