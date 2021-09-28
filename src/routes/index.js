e303812259bab17d8cad4082c788c944b9c39d26
Rasel Khan <rasel.khan@jmrltd.com>
2021-09-28 13:28:05 +0600

    add global request error handler

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,4 +6,11 @@ exports.init= (app)=> {
     app.use('/api/v1.0/products',productRoute);
     app.get('/favicon.ico', (req, res) => {return res.status(204).end()});
     app.get('/',(req,res)=>res.render('index'));
+    
+
+    //global error handler
+    const HTTP_SERVER_ERROR = 500;
+    app.use(function(err, req, res, next) {
+     res.status(err.status || HTTP_SERVER_ERROR).end();
+    });
 };
